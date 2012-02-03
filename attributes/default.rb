9bbe4a8a3a98de02fb52beb45093f279a0f48628
Peter Donald <peter@realityforge.org>
2012-02-03 15:12:47 +1100

    Enforce the use of authorization to access the mongo database

--- attributes/default.rb
+++ attributes/default.rb
@@ -6,7 +6,6 @@ default[:graylog2][:mongodb][:host] = "localhost"
 default[:graylog2][:mongodb][:port] = 27017
 default[:graylog2][:mongodb][:max_connections] = 150
 default[:graylog2][:mongodb][:database] = "graylog2"
-default[:graylog2][:mongodb][:auth] = "false"
 default[:graylog2][:mongodb][:user] = "user"
 default[:graylog2][:mongodb][:password] = "password"
 
