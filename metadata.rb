f4ee86368847d18c73bd39a69933969845024bde
Peter Donald <peter@realityforge.org>
2012-02-08 11:56:52 +1100

    Bump the version to development release

--- metadata.rb
+++ metadata.rb
@@ -2,7 +2,7 @@ maintainer        "Medidata Solutions Inc."
 maintainer_email  "cloudteam@mdsol.com"
 license           "Apache 2.0"
 description       "Installs and configures Graylog2"
-version           "0.0.6"
+version           "0.0.7"
 recipe            "graylog2", "Installs and configures Graylog2"
 
 # Only supporting Ubuntu 10.x
