5b8da34106ea5aa9bd539dd6811c04c8dadf96a3
Peter Donald <peter@realityforge.org>
2013-03-14 21:25:38 +1100

    Change quote style

--- metadata.rb
+++ metadata.rb
@@ -1,17 +1,17 @@
-name              "graylog2"
-maintainer        "Peter Donald"
-maintainer_email  "peter@realityforge.org"
-license           "Apache 2.0"
-description       "Installs and configures Graylog2"
-version           "0.0.7"
+name              'graylog2'
+maintainer        'Peter Donald'
+maintainer_email  'peter@realityforge.org'
+license           'Apache 2.0'
+description       'Installs and configures Graylog2'
+version           '0.0.7'
 
-recipe            "default", "Installs and configures Graylog2"
+recipe            'default', 'Installs and configures Graylog2'
 
 # Only supporting Ubuntu 10.x
-supports "ubuntu"
+supports 'ubuntu'
 
 # OpsCode cookbook dependencies
-depends "apt"
-depends "apache2"
-depends "java"
-depends "mongodb"
+depends 'apt'
+depends 'apache2'
+depends 'java'
+depends 'mongodb'
