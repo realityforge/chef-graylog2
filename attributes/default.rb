fd44f3df4a164e01f58474e2bef2244c9f81ded4
Peter Donald <peter@realityforge.org>
2013-03-14 21:27:19 +1100

    Add copyright headers

--- attributes/default.rb
+++ attributes/default.rb
@@ -1,3 +1,17 @@
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+
 default[:graylog2][:basedir] = "/var/graylog2"
 default[:graylog2][:server][:version] = "0.9.5p1"
 default[:graylog2][:web_interface][:version] = "0.9.5p2"
